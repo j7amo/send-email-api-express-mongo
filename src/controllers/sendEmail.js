0043cd04d6023c5edb672876c061131bff7d40ca
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-27 13:59:31 +0300

    set up real email sending via SendGrid service

--- src/controllers/sendEmail.js
+++ src/controllers/sendEmail.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const sgMail = require('@sendgrid/mail');
 const { StatusCodes } = require('http-status-codes');
 const nodemailer = require('nodemailer');
 
@@ -8,7 +10,8 @@ const nodemailer = require('nodemailer');
 // Messages are stored for few hours and then deleted
 // To see received/sent messages navigate to "https://ethereal.email/messages"
 // So basically this is just for the development purpose.
-const sendEmail = async (req, res) => {
+// eslint-disable-next-line no-unused-vars
+const sendEmailEthereal = async (req, res) => {
   // 1) create reusable transporter object using the default SMTP transport
   const transporter = nodemailer.createTransport({
     // if we decide to use popular email service such as Gmail
@@ -31,6 +34,32 @@ const sendEmail = async (req, res) => {
   });
 
   res.status(StatusCodes.OK).json(info);
+};
+
+// If we want to send EMAIL FOR REAL we can use multiple services.
+// For example, we can use SendGrid service.
+const sendEmail = async (req, res) => {
+  // to use it we need to import the package and set the API key (which we get on registration)
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  // then create an object with these fields:
+  const msg = {
+    to: process.env.EMAIL,
+    from: process.env.EMAIL,
+    subject: 'Sending with SendGrid is Fun',
+    text: 'Test 2',
+    html: '<strong>One more test</strong>',
+  };
+
+  let result;
+
+  try {
+    result = await sgMail.send(msg);
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.status(StatusCodes.OK).json(result);
 };
 
 module.exports = sendEmail;
